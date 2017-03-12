9259ff649d418b7d81a5bc45f8afba4a026318b7
stuartsy <stuartsy96@gmail.com>
2017-03-12 13:23:11 -0700

    initializing variables differently

--- code/similarity_model.py
+++ code/similarity_model.py
@@ -201,8 +201,10 @@ class SimilarityModel(Model):
                 ### END YOUR CODE
 
         ### YOUR CODE HERE (~2-4 lines)
-        logistic_a = tf.Variable(tf.zeros([1], dtype=tf.float32))
-        logistic_b = tf.Variable(tf.zeros([1], dtype=tf.float32))
+        logistic_a = tf.get_variable("a", (1,), tf.float32, tf.contrib.layers.xavier_initializer())
+        logistic_b = tf.get_variable("b", (1,), tf.float32, tf.constant_initializer(0))
+        # logistic_a = tf.Variable(tf.zeros([1], dtype=tf.float32))
+        # logistic_b = tf.Variable(tf.zeros([1], dtype=tf.float32))
         preds = tf.sigmoid(logistic_a * norm(h1 - h2) + logistic_b)
         # preds = (cosine_distance(h1, h2) + 1.0) / 2.0
         ### END YOUR CODE
@@ -341,4 +343,5 @@ class SimilarityModel(Model):
             if score > best_score:
                 best_score = score
                 print("New best score: %f" % best_score)
+            print ""
         return best_score

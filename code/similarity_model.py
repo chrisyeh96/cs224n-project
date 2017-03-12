47a7ca63673708619977b4751143233d42a1292f
stuartsy <stuartsy96@gmail.com>
2017-03-12 14:04:39 -0700

    remove print

--- code/similarity_model.py
+++ code/similarity_model.py
@@ -187,8 +187,8 @@ class SimilarityModel(Model):
 
         # h1 = tf.get_variable("h1", (tf.shape(x1)[0], self.config.hidden_size), tf.float32, tf.contrib.layers.xavier_initializer())
         # h2 = tf.get_variable("h2", (tf.shape(x1)[0], self.config.hidden_size), tf.float32, tf.contrib.layers.xavier_initializer())
-        self.h1 = h1
-        self.h2 = h2
+        # self.h1 = h1
+        # self.h2 = h2
         ### END YOUR CODE
 
         with tf.variable_scope("RNN") as scope:
@@ -314,10 +314,7 @@ class SimilarityModel(Model):
         for i, batch in enumerate(self.stupid_minibatch(train_examples, self.config.batch_size)):
             sentence1_batch, sentence2_batch, labels_batch = batch
             feed = self.create_feed_dict(sentence1_batch, sentence2_batch, labels_batch, dropout=self.config.dropout)
-            _, loss, h1, h2 = sess.run([self.train_op, self.loss, self.h1, self.h2], feed_dict=feed)
-            print("h1 and h2")
-            print(h1)
-            print(h2)
+            _, loss = sess.run([self.train_op, self.loss], feed_dict=feed)
             prog.update(i+1, [("train loss", loss)])
         print("")
 

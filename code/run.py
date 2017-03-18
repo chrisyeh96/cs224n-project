64082ad9fffd80135dabaedce2aa3ec59000d603
Yushi Homma <Yushi@DN51vs95.SUNet>
2017-03-18 00:01:22 -0700

    Changed run.py to include concat_steroids

--- code/run.py
+++ code/run.py
@@ -174,7 +174,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("-b", "--batch_size", type=int, required=False, help="number of examples for each minibatch")
     parser.add_argument("-c", "--cell", required=False, choices=["rnn", "gru"], help="model cell type")
-    parser.add_argument("-d", "--distance_measure", required=False, choices=["l2", "cosine", "custom_coef", "concat"], help="distance measure")
+    parser.add_argument("-d", "--distance_measure", required=False, choices=["l2", "cosine", "custom_coef", "concat", "concat_steroids"], help="distance measure")
     parser.add_argument("-r", "--regularization_constant", type=float, required=False, help="regularization constant")
     parser.add_argument("-hs", "--hidden_size", type=int, required=False, help="neural net hidden size")
     parser.add_argument("-ml", "--max_length", type=int, required=False, help="maximum length of sentences")

19d6549f7f4cddbd2671d2661ddcdf6e0cbe243f
Christopher Yeh <chrisyeh96@gmail.com>
2017-06-01 23:51:49 +0200

    Remove unused variable in convert_to_tsv.py and fix comment typo in data_preprocess.py

--- code/convert_to_tsv.py
+++ code/convert_to_tsv.py
@@ -1,13 +1,11 @@
 with open('train.tsv', 'w+') as new_f:
 	with open('train.csv') as f:
-		count = 10
 		for line in f:
 			split_line = line.split('\",\"')
 			new_f.write('\"\t\"'.join(split_line))
 
 with open('test.tsv', 'w+') as new_f:
 	with open('test.csv') as f:
-		count = 10
 		for line in f:
 			split_line = line.split('\",\"')
 			new_f.write('\"\t\"'.join(split_line))

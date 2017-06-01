20f4759647b023f88fb07b9811825bd1951d4ace
Yushi Homma <Yushi@DN51vt2b.SUNet>
2017-06-01 14:52:08 -0700

    fixed convert to tstv

--- code/convert_to_tsv.py
+++ code/convert_to_tsv.py
@@ -1,13 +1,23 @@
-with open('train.tsv', 'w+') as new_f:
-	with open('train.csv') as f:
+with open('../data/quora/train.tsv', 'w+') as new_f:
+	with open('../data/quora/train.csv') as f:
 		count = 10
 		for line in f:
 			split_line = line.split('\",\"')
 			new_f.write('\"\t\"'.join(split_line))
 
-with open('test.tsv', 'w+') as new_f:
-	with open('test.csv') as f:
+			if count == 0:
+				break
+
+			count -= 1
+
+with open('../data/quora/test.tsv', 'w+') as new_f:
+	with open('../data/quora/test.csv') as f:
 		count = 10
 		for line in f:
-			split_line = line.split('\",\"')
-			new_f.write('\"\t\"'.join(split_line))
+			first_comma_idx = line.index(',')
+			split_line = line[first_comma_idx + 1:].split('\",\"')
+			new_f.write(line[:first_comma_idx] + '\t' + '\"\t\"'.join(split_line))
+			if count == 0:
+				break
+
+			count -= 1

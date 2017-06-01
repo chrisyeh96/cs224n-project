49b2848c5a02083dc4d7de70465f7a6cef011692
stuartsy <stuartsy96@gmail.com>
2017-06-01 16:17:14 -0700

    full epochs

--- code/predict.sh
+++ code/predict.sh
@@ -1,3 +1,3 @@
 python run.py -a -c gru -d concat_steroids
-kg submit -c quora-question-pairs -u hommayushi3 -p tempUnsecure
+kg submit -c quora-question-pairs -u hommayushi3 -p tempUnsecure ../submission.csv
 # sudo poweroff
